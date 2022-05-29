1554eab7a9c78892661a80bfb4ffdc7ccaabd3ed
Fabien Marteau <mail@fabienm.eu>
2022-05-29 19:14:06 +0200

    adding spectre

--- discret.py
+++ discret.py
@@ -2,11 +2,11 @@ import numpy as np
 import pylab as plt
 
 # Freq
-f0 = 40 
+f0 = 1
 # Temps total
 T = 1
 # Nombre de points:
-N = 40
+N = 100
 # Fréquence d'échantillonage :
 print(f"fs = {N/T} Hertz")
 # 40 points de 0 à 39
@@ -15,7 +15,8 @@ y = np.sin(2*np.pi*f0*t)
 
 
 
-fix, ax = plt.subplots()
-ax.stem(t, y, 'b', markerfmt="b.")
+fix, ax = plt.subplots(1,2)
+ax[0].stem(t, y, 'b', markerfmt="b.")
+ax[1].magnitude_spectrum(y, Fs=N/T, ds="steps-mid")
 plt.show()
 
